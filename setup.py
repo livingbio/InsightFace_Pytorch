f97a767e758fdb8e2695c6ae4bb79a061e3053bf
Allen Wu <arashilen@gmail.com>
2019-02-13 16:46:30 +0800

    Packaging insight face

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 setup(
     name='insight-face-pytorch',
     setup_requires=['setuptools_scm'],
-    version='0.0.6',
+    version='0.0.8',
     packages=find_packages(),
     zip_safe=False,
 )
